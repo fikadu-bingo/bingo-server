9bb114d40a52c392b188af3e69b8c8724c518048
fikadu-bingo <fikadu.worku02@gmail.com>
2025-07-14 20:50:24 +0300

    Add files via upload
    update routes with game logic

--- routes/game.js
+++ routes/game.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const { createGame, buyTicket } = require("../controllers/gameController");
+const {
+  createGame,
+  buyTicket,
+  joinGame, // add joinGame controller function
+} = require("../controllers/gameController");
+
 const router = express.Router();
 
+// Create new game
 router.post("/create", createGame);
+
+// Buy ticket for a game
 router.post("/ticket", buyTicket);
+
+// Join a game (this is used by Telegram bot)
+router.post("/join", joinGame);
 
 module.exports = router;
