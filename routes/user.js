e29672e26a4e1aeb57a9c5b7e35f233e0488e5a5
fikadu-bingo <fikadu.worku02@gmail.com>
2025-07-21 13:23:47 +0300

    Add files via upload
    user updated maybe the 04 error can be solved

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,24 @@
 const express = require("express");
-const { telegramAuth, deposit, withdraw, transfer } = require("../controllers/userController");
+const multer = require("multer");
+const {
+  telegramAuth,
+  deposit,
+  withdraw,
+  transfer
+} = require("../controllers/userController");
+
 const router = express.Router();
 
+// Configure Multer for file uploads
+const storage = multer.memoryStorage(); // or use diskStorage if saving to disk
+const upload = multer({ storage });
+
+// Routes
 router.post("/telegram-auth", telegramAuth);
-router.post("/deposit", deposit);
+
+// If receipt is uploaded, expect receipt field from form-data
+router.post("/deposit", upload.single("receipt"), deposit);
+
 router.post("/withdraw", withdraw);
 router.post("/transfer", transfer);
 
