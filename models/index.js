0d22420d1e15b05f3c5db1e4d80c405be2588cb2
fikadu-bingo <fikadu.worku02@gmail.com>
2025-07-28 12:42:46 +0300

    Add files via upload
    index.js is updated

--- models/index.js
+++ models/index.js
@@ -1,8 +1,27 @@
+const sequelize = require("../config/db");
 const User = require("./user");
 const Game = require("./game");
+const Ticket = require("./ticket");
+const Deposit = require("./deposit");
+const Cashout = require("./cashout");
+const CalledNumber = require("./CalledNumber");
+// Define associations if needed
+User.hasMany(Ticket, { foreignKey: "user_id" });
+Ticket.belongsTo(User, { foreignKey: "user_id" });
 
-// Export all models here
+User.hasMany(Deposit, { foreignKey: "user_id" });
+Deposit.belongsTo(User, { foreignKey: "user_id" });
+
+User.hasMany(Cashout, { foreignKey: "user_id" });
+Cashout.belongsTo(User, { foreignKey: "user_id" });
+
+// Export all models
 module.exports = {
+  sequelize,
   User,
   Game,
+  Ticket,
+  Deposit,
+  Cashout,
+  CalledNumber,
 };
