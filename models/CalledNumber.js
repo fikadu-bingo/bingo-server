1de2a3a8a2f0d94e785d3151b75c5fa7c456ee77
fikadu-bingo <fikadu.worku02@gmail.com>
2025-07-28 14:00:24 +0300

    Update CalledNumber.js

--- models/CalledNumber.js
+++ models/CalledNumber.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
-const Game = require("./Game");
+const Game = require("./game");
 
 const CalledNumber = sequelize.define("CalledNumber", {
   id: {
